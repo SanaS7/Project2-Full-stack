650e20274ee83032028e3d2000017b9d3695c8ad
SanaS7 <126742643+SanaS7@users.noreply.github.com>
2023-07-09 20:52:01 +0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -24,17 +24,25 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-UserSchema.pre('save', async function (next) {
-  if (this.isModified('password')) {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-  }
-  next();
-});
-
 UserSchema.methods.checkPassword = async function (loginPw) {
-  return await bcrypt.compare(loginPw, this.password);
+  try {
+    return await bcrypt.compare(loginPw, this.password);
+  } catch (error) {
+    throw new Error('Password comparison failed');
+  }
 };
+
+UserSchema.pre('save', async function (next) {
+  try {
+    if (this.isModified('password')) {
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, salt);
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 const User = mongoose.model('User', UserSchema);
 
